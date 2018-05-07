a0885d809b0a524597011a7702c9e24990a343c7
souen <souen_xx@yahoo.co.jp>
2018-05-07 07:34:32 +0000

    resubmission

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,7 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :tasks, only:[:create,:destroy,:new,:show,:index]
+  resources :tasks, only:[:create,:destroy,:new,:edit]
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create] 
