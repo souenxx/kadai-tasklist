ec9505168414b953ca8974bac8fce97caf2e742c
souen <souen_xx@yahoo.co.jp>
2018-05-07 06:46:42 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,14 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page])
+    #@tasks = Task.all.page(params[:page])
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build  # form_for 用
+      @tasks = current_user.tasks.order('created_at').page(params[:page])
+    end
   end
 
   def show
@@ -13,14 +19,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Taskが正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが登録されませんでした'
-      render :new
+      render 'tasks/index'
     end
   end
 
@@ -39,9 +45,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :status, presence: true, length: { maximum: 10 }
   validates :content, presence: true, length: {maximum: 100}
+  validates :user_id, presence: true
+  
+  belongs_to :user
 end
